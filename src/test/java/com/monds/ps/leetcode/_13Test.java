1036f7f7b18ccc6d2f7ea0a6d3726b5df6a5d942
Sang Uk Ahn <suahnn@gmail.com>
2020-08-06 23:07:58 +0900

    Solved more questions.

--- src/test/java/com/monds/ps/leetcode/_13Test.java
+++ src/test/java/com/monds/ps/leetcode/_13Test.java
@@ -1,25 +1,25 @@
 package com.monds.ps.leetcode;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class _13Test {
 
     private _13.Solution solution;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         solution = new _13.Solution();
     }
 
     @Test
     public void test() {
-        assertEquals(solution.romanToInt("III"), 3);
-        assertEquals(solution.romanToInt("IV"), 4);
-        assertEquals(solution.romanToInt("IX"), 9);
-        assertEquals(solution.romanToInt("LVIII"), 58);
-        assertEquals(solution.romanToInt("MCMXCIV"), 1994);
+        assertThat(solution.romanToInt("III")).isEqualTo(3);
+        assertThat(solution.romanToInt("IV")).isEqualTo(4);
+        assertThat(solution.romanToInt("IX")).isEqualTo(9);
+        assertThat(solution.romanToInt("LVIII")).isEqualTo(58);
+        assertThat(solution.romanToInt("MCMXCIV")).isEqualTo(1994);
     }
 }
