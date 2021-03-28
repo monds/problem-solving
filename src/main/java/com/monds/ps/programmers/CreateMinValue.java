c2b5a34a40f95ab29acf0a4a05d24ee8cd18863a
Sang Uk Ahn <suahnn@gmail.com>
2021-03-28 23:36:59 +0900

    Solve more question

---
+++ src/main/java/com/monds/ps/programmers/CreateMinValue.java
@@ -0,0 +1,18 @@
+package com.monds.ps.programmers;
+
+import java.util.Arrays;
+
+public class CreateMinValue {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[A.length - i - 1];
+        }
+
+        return answer;
+    }
+}
