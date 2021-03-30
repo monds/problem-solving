5112a48b5fbd84776391924b08f2aa275f7e5181
monds <suahnn@gmail.com>
2021-03-30 15:30:56 +0900

    Solved more question

---
+++ src/main/java/com/monds/ps/programmers/_12949.java
@@ -0,0 +1,19 @@
+package com.monds.ps.programmers;
+
+public class _12949 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int m = arr1[0].length;
+        int n = arr2[0].length;
+        int[][] answer = new int[arr1.length][arr2.length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                for (int k = 0; k < m; k++) {
+                    sum += arr1[i][k] * arr2[k][j];
+                }
+                answer[i][j] = sum;
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/com/monds/ps/programmers/_12949Test.java
@@ -0,0 +1,24 @@
+package com.monds.ps.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class _12949Test {
+
+    @Test
+    void solution1() {
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+
+        assertThat(new _12949().solution(arr1, arr2)).isEqualTo(new int[][] {{15,15},{15,15},{15,15}});
+    }
+
+    @Test
+    void solution2() {
+        int[][] arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
+        int[][] arr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
+
+        assertThat(new _12949().solution(arr1, arr2)).isEqualTo(new int[][] {{22,22,11}, {36,28,18}, {29,20,14}});
+    }
+}
