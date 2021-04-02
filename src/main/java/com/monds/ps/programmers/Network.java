772b2c0a8a0bf48d4bbd93b531a7cdb382926134
monds <suahnn@gmail.com>
2021-04-02 20:34:47 +0900

    solve problem

--- src/main/java/com/monds/ps/programmers/Network.java
+++ src/main/java/com/monds/ps/programmers/Network.java
@@ -1,26 +1,27 @@
 package com.monds.ps.programmers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Network {
     public int solution(int n, int[][] computers) {
-        int count = 0;
-        boolean[] visit = new boolean[computers.length];
-        for (int i = 0; i < computers.length; i++) {
-            if (visit[i]) continue;
-            dfs(computers, visit, i, i);
-            count++;
+        int answer = 0;
+        int len = computers.length;
+        boolean[] visits = new boolean[len];
+        for (int i = 0; i < len; i++) {
+            if (visits[i]) continue;
+            dfs(computers, visits, i, i);
+            // 이어진 컴퓨터를 모두 찾고 1 증가
+            answer++;
         }
-        return count;
+        return answer;
     }
 
-    private void dfs(int[][] computers, boolean[] visit, int from, int to) {
+    private void dfs(int[][] computers, boolean[] visits, int from, int to) {
+        // from에서 to로 연결되었다면...
         if (computers[from][to] == 1) {
-            visit[to] = true;
+            visits[to] = true;
+            // 다른 컴퓨터로 이동 시도
             for (int i = 0; i < computers.length; i++) {
-                if (visit[i]) continue;
-                dfs(computers, visit, to, i);
+                if (visits[i]) continue;
+                dfs(computers, visits, to, i);
             }
         }
     }
