1ce3e52d8c0efe4369d5cfe1db54ec61b5b2246c
Sang Uk Ahn <suahnn@gmail.com>
2021-04-03 00:24:16 +0900

    Solve more question

--- src/main/java/com/monds/ps/programmers/StockPrice.java
+++ src/main/java/com/monds/ps/programmers/StockPrice.java
@@ -1,21 +1,14 @@
 package com.monds.ps.programmers;
 
 public class StockPrice {
-    static class Solution {
-        public int[] solution(int[] prices) {
-            int[] answer = new int[prices.length];
-            for (int i = 0; i < prices.length; i++) {
-                int curr = prices[i];
-                for (int j = i + 1; j < prices.length; j++) {
-                    if (curr <= prices[j]) {
-                        answer[i]++;
-                    } else {
-                        answer[i]++;
-                        break;
-                    }
-                }
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) break;
             }
-            return answer;
         }
+        return answer;
     }
 }
--- src/test/java/com/monds/ps/programmers/StockPriceTest.java
+++ src/test/java/com/monds/ps/programmers/StockPriceTest.java
@@ -8,7 +8,7 @@ public class StockPriceTest {
 
     @Test
     void test() {
-        StockPrice.Solution solution = new StockPrice.Solution();
+        StockPrice solution = new StockPrice();
         assertThat(solution.solution(new int[] {1, 2, 3, 2, 3})).isEqualTo(new int[] {4, 3, 1, 1, 0});
         assertThat(solution.solution(new int[] {1, 2, 3, 2, 3, 1})).isEqualTo(new int[] {5, 4, 1, 2, 1, 0});
     }
